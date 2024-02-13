3275e876759bbb9108961a341eecff8f7e1379a5
mustafa <121257754+mustafaakalin@users.noreply.github.com>
2024-02-13 23:23:38 +0300

    first and completed commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from datetime import datetime, timedelta
+import subprocess
+
+# Sınav Tarihi ve Saati
+sinav_tarihi = datetime(2024, 6, 18, 9, 0)
+
+# Güncel Zaman
+simdiki_zaman = datetime.now()
+
+# Kalan Süreyi Hesaplama
+kalan_sure = sinav_tarihi - simdiki_zaman
+
+# Pencere Oluşturma
+pencere = tk.Tk()
+pencere.geometry("800x480")
+pencere.title("Temel Yeterlilik Testi")
+
+# Arka Plan Rengi
+pencere.configure(bg="white")
+
+# Yazı Tipi
+yazi_tipi = ("Arial", 36, "bold")
+
+# Kırmızı Renk
+kirmizi = "#FF0000"
+
+# Kalan Süre Göstergesi
+kalan_sure_str = f"{kalan_sure.days} gün {kalan_sure.seconds//3600} saat {kalan_sure.seconds//60 % 60} dakika KALDI."
+kalan_sure_label = tk.Label(text=kalan_sure_str, font=yazi_tipi, fg=kirmizi, bg="white")
+kalan_sure_label.pack(padx=20, pady=20)
+
+# Sistem Başlatıldığında Otomatik Çalıştırma
+def otomatik_calistirma():
+    subprocess.call(["python", "Temel_Yeterlilik_Testi.py"])
+
+# Otomatik Çalıştırma Fonksiyonu Çağrısı
+otomatik_calistirma()
+
+# Pencereyi Gösterme
+pencere.mainloop()
